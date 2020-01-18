6a94394bf17211549de4243374c135976c53cc74
黄麒展 <757518403@qq.com>
2020-01-18 20:53:38 +0800

    音频编码

---
+++ FFM/test/音频编码/audio_encode.h
@@ -0,0 +1,14 @@
+//
+//  audio_encode.h
+//  FFM
+//
+//  Created by 黄麒展 on 2020/1/14.
+//  Copyright © 2020 8km. All rights reserved.
+//
+
+#ifndef audio_encode_h
+#define audio_encode_h
+
+#include <stdio.h>
+
+#endif /* audio_encode_h */
